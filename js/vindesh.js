0c75e4e7e44b87b91aa08fc4a6b78d53662518c7
Shahinur Islam <shahinur04042002@gmail.com>
2023-02-25 15:13:47 +0600

    no capital

--- js/vindesh.js
+++ js/vindesh.js
@@ -1,6 +1,26 @@
 const loadData=()=>{
 fetch('https://restcountries.com/v3.1/all')
 .then(res=>res.json())
-.then(data=>console.log(data))
+.then(data=>displayCountries(data))
+}
+const displayCountries=countries=>{
+   const divCountries=document.getElementById('div-container')
+    console.log(countries);
+
+    // for(country of countries)
+    //     console.log(country)
+    // }
+
+    countries.forEach(country =>{
+        console.log(country);
+        const countryDiv=document.createElement('div');
+        countryDiv.classList.add('country.name.common');
+
+        countryDiv.innerHTML=`
+        <h3>Name: ${country.name.common}</h3>
+        <p>Capital: ${country.capital ? country.capital[0]:'no capital'}</p>
+        `;
+        divCountries.appendChild(countryDiv)
+    })
 }
 loadData();
