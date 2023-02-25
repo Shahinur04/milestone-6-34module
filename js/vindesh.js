c7979ad0a0911e5883610f3feddbb5039a5a3f65
Shahinur Islam <shahinur04042002@gmail.com>
2023-02-26 01:10:27 +0600

    rabbbana afuafa inni

--- js/vindesh.js
+++ js/vindesh.js
@@ -18,8 +18,27 @@ const displayCountries=countries=>{
         countryDiv.innerHTML=`
         <h3>Name: ${country.name.common}</h3>
         <p>Capital: ${country.capital ? country.capital[0]:'no capital'}</p>
+        <button onclick="countryDetails('${country.cca2}')">details</button>
+
         `;
         divCountries.appendChild(countryDiv)
     })
+}
+const countryDetails=code=>{
+    
+    // https://restcountries.com/v3.1/all
+const url=`https://restcountries.com/v3.1/alpha/${code}`;
+console.log(url);
+fetch(url)
+.then(res=>res.json())
+.then(data=>showCountry(data[0]));
+
+}
+const showCountry=country=>{
+    console.log(country)
+    const detailContainer=document.getElementById('section-detail');
+    detailContainer.innerHTML=`<h2>Country detail</h2>
+    <h3>Name:${country.name.common}</h3>
+    <img src="${country.flags.png}" alt="">`
 }
 loadData();
